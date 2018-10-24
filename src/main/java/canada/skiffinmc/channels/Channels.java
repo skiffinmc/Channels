e19eae311d926e1a0328e4c60a9ced33eeb32b82
BuildTools <unconfigured@null.spigotmc.org>
2018-10-24 18:31:29 -0400

    Initialize

---
+++ src/main/java/canada/skiffinmc/channels/Channels.java
@@ -0,0 +1,16 @@
+package canada.skiffinmc.channels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Channels extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
